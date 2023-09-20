dbc0623a8dc3d15162f3db80928eac4aa288813f
owstork94 <owstork94@gmail.com>
2023-09-20 15:21:03 +0900

    concret config 수정

--- src/main/java/hello/proxy/config/v1_proxy/concret_proxy/ConcreteProxyConfig.java
+++ src/main/java/hello/proxy/config/v1_proxy/concret_proxy/ConcreteProxyConfig.java
@@ -2,6 +2,9 @@ package hello.proxy.config.v1_proxy.concret_proxy;
 
 
 import hello.proxy.app.v1.*;
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
 import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
 import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
 import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
@@ -13,20 +16,20 @@ import org.springframework.context.annotation.Configuration;
 public class ConcreteProxyConfig {
 
     @Bean
-    public OrderControllerV1 orderController(LogTrace logTrace){
-        OrderControllerV1Impl controllerImpl = new OrderControllerV1Impl(orderService(logTrace));
-       return new OrderControllerInterfaceProxy(controllerImpl,logTrace);
+    public OrderControllerV2 orderController(LogTrace logTrace){
+        OrderControllerV2 controller = new OrderControllerV2(orderServiceV2(logTrace));
+       return new OrderControllerConcreteProxy(controller,logTrace);
     }
 
     @Bean
-    public OrderServiceV1 orderService(LogTrace logTrace) {
-        OrderServiceV1Impl orderServiceimpl = new OrderServiceV1Impl(orderRepository(logTrace));
-        return new OrderServiceInterfaceProxy(orderServiceimpl,logTrace);
+    public OrderServiceV2 orderServiceV2(LogTrace logTrace) {
+        OrderServiceV2 orderService = new OrderServiceV2(orderRepositoryV2(logTrace));
+        return new OrderServiceConcreteProxy(orderService,logTrace);
     }
 
     @Bean
-    public OrderRepositoryV1 orderRepository(LogTrace logTrace) {
-        OrderRepositoryV1 orderRepositoryImpl = new OrderRepositoryV1Impl();
-        return new OrderRepositoryInterfaceProxy(orderRepositoryImpl,logTrace);
+    public OrderRepositoryV2 orderRepositoryV2(LogTrace logTrace) {
+        OrderRepositoryV2 orderRepository = new OrderRepositoryV2();
+        return new OrderRepositoryConcreteProxy(orderRepository,logTrace);
     }
 }
